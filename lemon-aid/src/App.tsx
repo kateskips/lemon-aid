8c4358f6dc2823402a803639b2707f59798c4b0b
Kate Follett <git@klgf.dev>
2023-02-27 13:19:30 +0800

    ant design - layout, card, button, spin components

--- lemon-aid/src/App.tsx
+++ lemon-aid/src/App.tsx
@@ -1,15 +1,20 @@
+import { Layout, Card, Button, Spin } from 'antd';
 import { Advice } from './Advice';
 import { fetchAdvice } from './AdviceApi';
 
+const { Header, Content } = Layout;
 
 function App() {
 
   return (
+    <Layout className="layout">
+    <Header>
+    </Header>
     <div className="App">
       <header className="App-header">
         </header>
     </div>
-  )
+  </Layout>
 }
 
 export default App
