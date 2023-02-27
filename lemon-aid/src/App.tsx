e44b4769ad9fa6e8ec24de421cf2d36eed8b0a13
Kate Follett <git@klgf.dev>
2023-02-27 17:38:58 +0800

    getting the advice and the button event handler

--- lemon-aid/src/App.tsx
+++ lemon-aid/src/App.tsx
@@ -24,11 +24,19 @@ function App() {
     <Content className="content">
     <div className="App">
       <Card style={{margin: '0 auto'}}title="Random Advice Generator">
+        {advice ? <p>{advice.advice}</p> : <p>No advice yet</p>}
+        {loading ? (
           <Spin size="large" tip="Loading..." />
+        ) : (
+          <Button type="primary" onClick={getNewAdvice}>
+            Shoot
+          </Button>
+        )}
       </Card>
     </div>
     </Content>
   </Layout>
+  );
 }
 
 export default App
